3bcad003e7807fd205d10f1cff5ab3eb99682a9f
Patrick Mims <pmims1@mail.sfsu.edu>
2022-09-18 16:34:29 -0700

    Initial Commit

---
+++ Shared/ISYS_556_Food_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ISYS_556_Food_AssignmentApp.swift
+//  Shared
+//
+//  Created by Patrick Kevin Mims on 9/18/22.
+//
+
+import SwiftUI
+
+@main
+struct ISYS_556_Food_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
